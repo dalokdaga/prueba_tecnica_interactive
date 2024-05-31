d83b7c00040b4141ea31f275f10720501e6764d0
Daniel Garcia Uscanga <daniel.garcia@s4learning.com>
2024-05-30 20:42:35 -0600

    se elimina metodo que se creo pero termino siendo no utilizado

--- app/core/utilities.py
+++
@@ -1,8 +0,0 @@
-from fastapi import HTTPException
-
-
-def is_valid_enum(value, class_enum):
-    try:
-        class_enum(value)
-    except ValueError:
-        raise HTTPException(status_code=404, detail="Status Value Error")
